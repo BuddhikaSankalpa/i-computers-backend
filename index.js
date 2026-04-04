cc537cb7d3ebbaa61333e4dcb42031f59cfb4132
BuddhikaSankalpa <Buddhikasankalpa2003@gmail.com>
2026-04-04 23:10:10 +0530

    Initialize Express server and connect MongoDB using dotenv ✏️

---
+++ index.js
@@ -0,0 +1,26 @@
+import express from 'express'
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+dotenv.config()
+
+const mongoDBURI = process.env.MONGODB_URI
+
+mongoose.connect(mongoDBURI).then(
+    ()=>{
+        console.log("Connected to MongoDB successfully")
+    }
+)
+
+const app = express()
+
+app.use( express.json() )
+
+app.listen(
+    3000 ,
+    ()=>{
+        console.log('Server started successfully')
+        console.log('Listening on port 3000')
+    }
+)
+
+
